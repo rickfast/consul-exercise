fdb20136b4af34259ee317984855ddc082afbef7
rickfast <rick.t.fast@gmail.com>
2016-09-23 10:30:19 -0500

    clean up

--- src/main/java/xtech/FlakyService.java
+++ src/main/java/xtech/FlakyService.java
@@ -2,10 +2,8 @@ package xtech;
 
 import com.orbitz.consul.Consul;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
-import java.net.ConnectException;
 import java.net.URISyntaxException;
 
 @Service
@@ -16,7 +14,6 @@ public class FlakyService {
 
     }
 
-    @Retryable(ConnectException.class)
     public Response getData() throws URISyntaxException {
         return null;
     }
