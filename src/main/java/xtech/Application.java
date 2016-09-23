6adb5ee552e54c5551b98bef1de9df130bc06da2
rickfast <rick.t.fast@gmail.com>
2016-09-23 10:30:35 -0500

    clean up part 2

--- src/main/java/xtech/Application.java
+++ src/main/java/xtech/Application.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableRetry
 public class Application {
 
     @Bean
